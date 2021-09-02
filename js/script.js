d39035aefc43181df77f647e5a89123298fde401
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-09-02 22:47:48 +0200

    stampa singola email "prova"

--- js/script.js
+++ js/script.js
@@ -3,6 +3,46 @@ Vue.config.devtools = true;
 
 const app = new Vue({
     el: "#app",
-    data: {},
+    data: {
+        mail: ""
+
+    },
     methods: {},
-})
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
+            this.mail = res.data.response;
+        });
+    },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
